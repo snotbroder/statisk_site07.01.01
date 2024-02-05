e5629f838b4f268192997af075b44d634722fdfd
Joris <jote0002@stud.kea.dk>
2024-02-05 14:52:09 +0100

    givet mulighed for at aflæse id på produkt.js

--- produkt.js
+++ produkt.js
@@ -1,4 +1,7 @@
-const url = "https://kea-alt-del.dk/t7/api/products/";
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+const url = "https://kea-alt-del.dk/t7/api/products/" + id;
 
 fetch(url)
   .then((response) => response.json())
